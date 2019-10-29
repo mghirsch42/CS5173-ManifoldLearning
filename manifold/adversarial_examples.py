b78c71236234d22985e1212416052f86706fe25b
mghirsch42 <hirschmg42@gmail.com>
2019-10-29 12:06:00 -0500

    mnist adversarial examples, errors

---
+++ manifold/adversarial_examples.py
@@ -0,0 +1,108 @@
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from keras.models import load_model
+from matplotlib import pyplot as plt
+import tensorflow as tf
+
+import os
+
+# from https://keras.io/examples/mnist_cnn/
+def generate_model(): 
+    batch_size = 128
+    num_classes = 10
+    epochs = 12
+
+    # input image dimensions
+    img_rows, img_cols = 28, 28
+
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                    activation='relu',
+                    input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer=keras.optimizers.Adadelta(),
+                metrics=['accuracy'])
+
+    model.fit(x_train, y_train,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=1,
+            validation_data=(x_test, y_test))
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+    return model
+
+def generate_adversarial(input_image, input_label, model):
+    # from https://www.tensorflow.org/tutorials/generative/adversarial_fgsm
+    input_image = tf.convert_to_tensor([input_image])
+    input_image = tf.transpose(input_image, (1, 2, 0))
+    print(tf.rank(input_image))
+    print(tf.shape(input_image))
+
+    loss_object = tf.keras.losses.CategoricalCrossentropy()
+
+    with tf.GradientTape() as tape:
+        tape.watch(input_image)
+        prediction = model(input_image)
+        loss = loss_object(input_label, prediction)
+
+    # Get the gradients of the loss w.r.t to the input image.
+    gradient = tape.gradient(loss, input_image)
+    # Get the sign of the gradients to create the perturbation
+    signed_grad = tf.sign(gradient)
+    return signed_grad
+
+def main():
+    if os.path.exists("mnist_model.h5"):    
+        model = load_model('mnist_model.h5')
+
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        base_example = x_test[0]
+        base_label = y_test[0]
+
+        generate_adversarial(base_example, base_label, model)
+
+    else:
+        model = generate_model()
+        model.save("mnist_model.h5")
+
+if __name__ == "__main__":
+    main()
