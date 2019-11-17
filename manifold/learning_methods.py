9cd4ec12fd0ddcbc1b8b31b97d370d5788786e85
MG Hirsch <mghirsch@ou.edu>
2019-11-17 10:31:01 -0600

    add learning methods and do some setup (#11)

---
+++ manifold/learning_methods.py
@@ -0,0 +1,24 @@
+from sklearn.manifold import LocallyLinearEmbedding
+from sklearn.manifold import Isomap
+from sklearn.manifold import SpectralEmbedding
+
+
+def lle(x):
+    """
+    https://stackoverflow.com/questions/42275922/setting-the-parameters-of-locally-linear-embedding-lle-method-in-scikit-learn
+    """
+    embedding = LocallyLinearEmbedding(n_components=2)  # 2D projection
+    x_transformed = embedding.fit_transform(x)
+    return x_transformed
+
+
+def isomap(x):
+    embedding = Isomap(n_components=2)
+    x_transformed = embedding.fit_transform(x)
+    return x_transformed
+
+
+def laplacian(x):
+    embedding = SpectralEmbedding(n_components=2)
+    x_transformed = embedding.fit_transform(x)
+    return x_transformed
